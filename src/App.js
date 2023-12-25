b569de631fa9301abd4678067315b494b3618791
RishEverec <117337128+rish4522@users.noreply.github.com>
2023-12-26 01:19:06 +0530

    React Components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import './App.css';
+import Heading from './Heading';
+import FoodItems from './FoodItems';
 
 function App() {
   return (
     <>
     <div className='main-container'>
-      <h1 className="heading-1" draggable>My Favourite Foods</h1>
-      <ul className='favList'>
-        <div className='list-item'>
-          <img src='https://e0.pxfuel.com/wallpapers/389/311/desktop-wallpaper-food-graphy-pizza.jpg' alt='Pizza'></img>
-          <li>Pizza</li>
-        </div>
-        <div className='list-item'>
-          <img src='https://wallpapercave.com/wp/wp11934989.jpg' alt='Soya Chaap'></img>
-          <li>Soya Chaap</li>
-        </div>
-        <div className='list-item'>
-          <img src='https://t3.ftcdn.net/jpg/04/94/92/18/360_F_494921881_UCVG7kgihpMUbbDC5VxuufeI7B5TQBr0.jpg' alt='Pani Puri'></img>
-          <li>Pani Puri</li>
-        </div> 
-      </ul>
+      <Heading />
+      <FoodItems />
     </div>
     </>
   );
