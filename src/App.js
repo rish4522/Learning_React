4bf040ac1465edcdb0672d7d0a394c3fd82cb4de
RishEverec <117337128+rish4522@users.noreply.github.com>
2024-01-10 13:19:57 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import './App.css';
 import Heading from './Heading';
 import FoodItems from './FoodItems';
 import Greetings from './Greetings';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
   return (
     <>
     <div className='main-container'>
       <Greetings />
       <Heading />
       <FoodItems />
+    </div>
+    <div className='state-container'>
+      <h1>{count}</h1>
+      <div className='buttons'>
+        <button onClick={() => {setCount(count + 1)}}>+</button>
+        <button onClick={() => {setCount(count - 1)}}>-</button>
+      </div>
     </div>
     </>
   );
--- src/FoodItems.jsx
+++ src/FoodItems.jsx
@@ -1,19 +1,42 @@
 import React from "react";
 
+const items = [
+  {
+    name : "Pizza",
+    image : "https://e0.pxfuel.com/wallpapers/389/311/desktop-wallpaper-food-graphy-pizza.jpg"
+  },
+  {
+    name : "Soya Chaap",
+    image : "https://wallpapercave.com/wp/wp11934989.jpg"
+  },
+  {
+    name : "Pani Puri",
+    image : "https://t3.ftcdn.net/jpg/04/94/92/18/360_F_494921881_UCVG7kgihpMUbbDC5VxuufeI7B5TQBr0.jpg"
+  }
+]
+
 function Card(props) {
   return (
     <div className='list-item'>
-      <img src={props.image} alt='Food Image'></img>
+      <img src={props.image} alt='Food Items'></img>
       <li>{props.name}</li>
     </div>
   );
 }
 
+function createCard(items) {
+  return (
+    <Card
+      key = {items.name}
+      image = {items.image}
+      name = {items.name}
+    />
+  );
+}
+
 function FoodItems() {
     return <ul className='favList'>
-      <Card name = "Pizza" image = "https://e0.pxfuel.com/wallpapers/389/311/desktop-wallpaper-food-graphy-pizza.jpg" />
-      <Card name = "Soya Chaap" image = "https://wallpapercave.com/wp/wp11934989.jpg" />
-      <Card name = "Pani Puri" image = "https://t3.ftcdn.net/jpg/04/94/92/18/360_F_494921881_UCVG7kgihpMUbbDC5VxuufeI7B5TQBr0.jpg" />
+      {items.map(createCard)}
   </ul>
 }
 
